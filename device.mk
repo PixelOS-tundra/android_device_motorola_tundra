63fe13044cf9cbc5337663db4fd2c70794b06b47
raghavt20 <raghavt20@aospa.co>
2026-03-31 22:53:30 +0530

    tundra: Enable UFFD GC support
    Change-Id: Ie3ef040469ccaa1e2927118da89ae49dd7a75191

--- device.mk
+++ device.mk
@@ -254,6 +254,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+OVERRIDE_ENABLE_UFFD_GC := true
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
