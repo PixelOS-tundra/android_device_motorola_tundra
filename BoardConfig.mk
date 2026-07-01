f5a747d119b4894f66fb82e87bd7f35d05f55234
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:32:24 -0400

    tundra: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,6 +149,7 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
--- device.mk
+++ device.mk
@@ -249,6 +249,9 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
+# ION
+$(call soong_config_set_bool,libion,legacy_impl,true)
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
